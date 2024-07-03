b8f5a8a183a12c5c2ef6b781825ec539e6129336
harshawije-dev <55786726+harshawije-dev@users.noreply.github.com>
2024-07-04 01:58:54 +0530

    mapbox update

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import mapboxgl from "mapbox-gl";
 import React from "react";
 
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_TOKEN!;
+
 function LocationPage() {
+  const mapContainer = React.useRef(null);
+  const map = React.useRef(null);
+
+  const [lng, setLng] = React.useState(-70.9);
+  const [lat, setLat] = React.useState(42.35);
+  const [zoom, setZoom] = React.useState(9);
+
+  React.useEffect(() => {}, []);
+  
   return <div>LocationPage</div>;
 }
 
